bfc2ca7a98db4f30f39c14ec4ce239620be14d5c
Lowt <you@example.com>
2020-10-14 16:35:13 +0800

    歌曲列表

--- public/appConfig.js
+++ public/appConfig.js
@@ -4,7 +4,7 @@
  * @Autor: Lowt
  * @Date: 2020-09-29 11:13:23
  * @LastEditors: Lowt
- * @LastEditTime: 2020-10-13 18:55:50
+ * @LastEditTime: 2020-10-14 15:43:34
  */
 var BASE_URL = 'http://localhost:9999' //接口请求地址
-var ICON_URL = '//at.alicdn.com/t/font_2127400_vh2dzxatsnf.js' //ico地址
+var ICON_URL = '//at.alicdn.com/t/font_2127400_4ybzniiwf26.js' //ico地址
--- src/views/search/model/tabModel.tsx
+++ src/views/search/model/tabModel.tsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 // import { RouteComponentProps } from "react-router";
 
-import {  List } from 'antd'
+import { List } from 'antd'
 import { createFromIconfontCN } from '@ant-design/icons';
 import pageApi from '../../../api/searchApi'
 
 const MyIcon = createFromIconfontCN({
     scriptUrl: (window as any).ICON_URL
-  });
-
+});
 
 
 
@@ -47,22 +46,40 @@ class PageModel extends Component<componentInter, any> {
 
                     dataSource={this.props.data}
                     renderItem={(item: any) => (
-                        <List.Item onClick={() => this.playSong(item.id)}>
+                        <List.Item>
                             <List.Item.Meta
-
-                                // title={<a href="https://ant.design">{item.name}</a>}
-
                                 description={
                                     <div className='song-item'>
-                                        <p className='song-name'><MyIcon type='iconbofang_huaban'/>{item.name}</p>
-                                        <p className='singer'>{item.artists.map((it: { name: string }, index: number) => {
+                                        <span className="play" title='播放'><MyIcon onClick={() => this.playSong(item.id)} type='iconbofang_active_huaban' /></span>
+                                        <p className='song-name'>
+                                            <a className='real-name' href={`/song?id=${item.id}`} title={item.name + item.alias.map((it: string, idx: number) => {
+                                                if (idx === 0) {
+                                                    return ' - ' + it
+                                                }
+                                                return '/' + it
+                                            })}>{item.name}</a>
+                                            <span className='alias'>{item.alias.map((it: string, idx: number) => {
+                                                if (idx === 0) {
+                                                    return ' - ' + it
+                                                }
+                                                return '/' + it
+                                            })}</span>
+                                        </p>
+                                        <p className="song-oper">
+                                            <span className='oper-item' title='添加到播放列表'><MyIcon type='iconjiahao' /></span>
+                                            <span className='oper-item' title='收藏'><MyIcon type='iconshoucang' /></span>
+                                            <span className='oper-item' title='分享'><MyIcon type='iconfenxiang' /></span>
+                                            <span className='oper-item' title='下载'><MyIcon type='iconxiazai' /></span>
+                                        </p>
+                                        <p className='singer'>{item.artists.map((it: { name: string, id: number }, index: number) => {
                                             if (index === 0) {
-                                                return it.name
+                                                return <a className='singer-item' key={it.id} href={"/artist?id=" + it.id}>{it.name}</a>
                                             } else {
-                                                return "/" + it.name
+                                                return <a className='singer-item' key={it.id} href={"/artist?id=" + it.id}>{"/" + it.name}</a>
                                             }
                                         })}</p>
-                                        <p className='album'>《{item.album.name}》</p>
+                                        <p className='album'><a title={item.album.name} href={"/album?id=" + item.album.id}>《{item.album.name}》</a> </p>
+                                        <p className="time">{this.dealSongTime(item.duration)}</p>
                                     </div>
                                 }
                             />
@@ -75,6 +92,16 @@ class PageModel extends Component<componentInter, any> {
 
 
         return ele
+    }
+
+    // 歌曲时间处理
+    private dealSongTime(data: number) {
+        let time = null
+        
+        var minutes = parseInt((data  / (1000 * 60)) as any);
+        var seconds = Math.round((data % (1000 * 60)) / 1000);
+        time = (minutes < 10 ? ('0' + minutes) : minutes) + ':' + (seconds < 10 ? ('0' + seconds) : seconds)
+        return time
     }
 
     //播放歌曲
